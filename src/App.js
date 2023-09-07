b053193825be1c4d6cac54221cbf903b5510ab52
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-09-07 18:04:27 +0530

    Add notifications to the webApp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { AlbumContainer } from "./Components/AlbumContainer/AlbumContainer";
 import { AlbumForm } from "./Components/AlbumForm/AlbumForm";
 import { NavBar } from "./Components/NavBar/NavBar";
+import { ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   let showForm = false;
   return (
     <div className="App">
+      <ToastContainer />
       <NavBar />
-      {showForm ? <AlbumForm/> : <AlbumContainer/>}
-
+      {showForm ? <AlbumForm /> : <AlbumContainer />}
     </div>
   );
 }
